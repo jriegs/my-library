e1c7f1c6ea9bfe4bab973d0fd88525f8740cc80f
julianriegler <rockdown1236@gmail.com>
2021-04-17 09:48:07 -0500

    add open/close functionality for modal form

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log('connected');
+let myLibrary = ['Book1', 'Book2', 'Book3'];
+
+function Book(title, author, pages) {
+  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+
+function displayLibrary(booksArr) {
+  for (book in booksArr) {
+    console.log(booksArr[book]);
+  }
+}
+
+displayLibrary(myLibrary);
+
+
+function triggerModal() {
+  const modal = document.querySelector('.js-modal');
+  modal.classList.toggle('hide');
+}
+
+
+/* Global */
+
+
+const addBookBtn = document.querySelector('.js-add-book');
+const modalBg = document.querySelector('.js-modal-bg');
+const modalCloseIcon = document.querySelector('.js-close-icon');
+  
+addBookBtn.addEventListener('click', triggerModal);
+modalBg.addEventListener('click', triggerModal);
+modalCloseIcon.addEventListener('click', triggerModal);
