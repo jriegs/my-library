6a1f3ffde7eefb4b586e64b0419003582384f237
julianriegler <rockdown1236@gmail.com>
2021-05-04 17:21:56 -0500

    update ui on book entry and add btn events

--- index.js
+++ index.js
@@ -5,6 +5,44 @@ function triggerModal() {
   modal.classList.toggle('hide');
 }
 
+function updateUI() {
+  const entryText = document.getElementById('entry-text');
+
+  if (myLibrary.length !== 0) {
+    entryText.style.display = 'none';
+  } else {
+    entryText.style.display = 'block';
+  }
+}
+
+function setBtnEvents(btns, eventType, fx) {
+  btns.forEach(btn => {
+    btn.addEventListener(eventType, fx);
+  });
+}
+
+function changeBookStatus() {
+  console.log('this will change the selected book\'s read status');
+}
+
+function editBook() {
+  console.log('this will edit the selected book')
+}
+
+function deleteBook() {
+  console.log('this will delete the selected book');
+}
+
+function addBtnEvents() {
+  const changeStatusBtns = document.querySelectorAll('.js-book-status');
+  const editBookBtns = document.querySelectorAll('.js-edit-book');
+  const deleteBookBtns = document.querySelectorAll('.js-delete-book');
+
+  setBtnEvents(changeStatusBtns, 'click', changeBookStatus);
+  setBtnEvents(editBookBtns, 'click', editBook);
+  setBtnEvents(deleteBookBtns, 'click', deleteBook);
+}
+
 function Book(title, author, pages, readStatus) {
   // the constructor...
   this.title = title;
@@ -20,54 +58,6 @@ function Book(title, author, pages, readStatus) {
     }
   }
 }
-
-function addBookToLibrary(e) {
-  // prevent default form submission
-  e.preventDefault();
-
-  // get and validate form submission data
-  const titleInput = document.querySelector('.js-title');
-  const authorInput = document.querySelector('.js-author');
-  const pagesInput = document.querySelector('.js-pages');
-  const readStatusInput = document.querySelector('.js-status');
-  let readStatus = false;
-  const errorText = document.querySelector('.js-error');
-
-  if (titleInput.value === '' ||
-      authorInput.value === '' || 
-      pagesInput.value <= 0
-  ) {
-    // check book values exist, throw error if no value
-    errorText.textContent =  'Please make sure entries are valid';
-    return
-  } else {
-    errorText.textContent = '';
-  }
-
-  // get read status value 
-  if (readStatusInput.checked) {
-    readStatus = true;
-  }
-
-  // if data is valid, create new book (object)
-  const newBook = new Book(titleInput.value, authorInput.value, pagesInput.value, readStatus);
-
-  // add new book to my library
-  myLibrary.push(newBook);
-
-  // display updated library
-  displayLibrary(myLibrary);
-
-  // clear inputs 
-  titleInput.value = '';
-  authorInput.value = '';
-  pagesInput.value = '';
-  readStatusInput.checked = false;
-
-  // close modal
-  triggerModal();
-}
-
 
 function displayLibrary(booksArr) {
   const bookTemplate = document.getElementById('bookTemplate');
@@ -101,6 +91,59 @@ function displayLibrary(booksArr) {
   })
 }
 
+function addBookToLibrary(e) {
+  // prevent default form submission
+  e.preventDefault();
+
+  // get and validate form submission data
+  const titleInput = document.querySelector('.js-title');
+  const authorInput = document.querySelector('.js-author');
+  const pagesInput = document.querySelector('.js-pages');
+  const readStatusInput = document.querySelector('.js-status');
+  let readStatus = false;
+  const errorText = document.querySelector('.js-error');
+
+  if (titleInput.value.trim() === '' ||
+      authorInput.value.trim() === '' || 
+      pagesInput.value <= 0
+  ) {
+    // check book values exist, throw error if no value
+    errorText.textContent =  'Please make sure entries are valid';
+    return
+  } else {
+    errorText.textContent = '';
+  }
+
+  // get read status value 
+  if (readStatusInput.checked) {
+    readStatus = true;
+  }
+
+  // if data is valid, create new book (object)
+  const newBook = new Book(titleInput.value, authorInput.value, pagesInput.value, readStatus);
+
+  // add new book to my library
+  myLibrary.push(newBook);
+
+  // clear inputs 
+  titleInput.value = '';
+  authorInput.value = '';
+  pagesInput.value = '';
+  readStatusInput.checked = false;
+
+  // close modal
+  triggerModal();
+
+  // update UI
+  updateUI();
+
+  // display updated library
+  displayLibrary(myLibrary);
+
+  // add btn events
+  addBtnEvents();
+}
+
 
 // modal variables
 const addBookBtn = document.querySelector('.js-add-book');
@@ -115,3 +158,6 @@ modalCloseIcon.addEventListener('click', triggerModal);
 // submit modal form to create new book card
 const addBookForm = document.querySelector('.js-form');
 addBookForm.addEventListener('submit', addBookToLibrary);
+
+// book variables
+const readStatus = document.querySelector('.js-book-status');
